ea30a9cbb5a41b6a82092017ec322339f27d63fa
suraj-verma2004 <147896230+suraj-verma2004@users.noreply.github.com>
2025-06-20 09:45:58 +0530

    Update index.js
    comments added to describe the project

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -8,19 +8,19 @@ const io = require("socket.io")(3000, {
 const users={};
 
 io.on('connection', socket=>{
-   socket.on('new-user-joined',uname=>{
+   socket.on('new-user-joined',uname=>{    //tracks when any new user joined the chat
 
     console.log("New user",uname);
-    users[socket.id]=uname;
+    users[socket.id]=uname;                //add the new user to the users list
 
-    socket.broadcast.emit('user-joined',uname);
+    socket.broadcast.emit('user-joined',uname);       //broadcast the message that the xyz user joined
    });
 
    socket.on('send',message=>{
-    socket.broadcast.emit('receive',{ message :message ,uname: users[socket.id]})
+    socket.broadcast.emit('receive',{ message :message ,uname: users[socket.id]})       //brodcasting the message sended by any user
    });
-   socket.on('disconnect',message=>{
+   socket.on('disconnect',message=>{               //to specify when any user left the chat
     socket.broadcast.emit('left',users[socket.id]);
-    delete users[socket.id];
+    delete users[socket.id];                     //delete the user name from users list when left
    });
 })
