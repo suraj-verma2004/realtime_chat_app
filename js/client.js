4c54cfcce4eaedf8b17b32a85cba8f17efe62845
suraj-verma2004 <147896230+suraj-verma2004@users.noreply.github.com>
2025-06-20 09:53:41 +0530

    Update client.js
    updated the comments to make code understandable

--- js/client.js
+++ js/client.js
@@ -1,43 +1,42 @@
-const socket = io('http://localhost:3000');
+const socket = io('http://localhost:3000');                  //connect my localhost Port to web socket
 
+//getting elements from html page
 const form=document.getElementById('send_container');
 const messageInput=document.getElementById('messageInp')
 const messageContainer=document.querySelector(".container")
-var audio=new Audio('beep.mp3');
+var audio=new Audio('beep.mp3');   
 
-const append=(message,position)=>{
+const append=(message,position)=>{                            //adding the message in the client side chatbox
       const messageEle=document.createElement('div');
       messageEle.innerText=message;
       messageEle.classList.add('message');
       messageEle.classList.add(position);
       messageContainer.append(messageEle);
-      if(position=='left'){
-      audio.play();
+      
+      if(position=='left'){                                  //used audio to beep when message received
+      audio.play();         
 }}
 
 const uname =prompt("Enter Your Name to join");
-socket.emit('new-user-joined',uname);
+socket.emit('new-user-joined',uname);                         //asking the user name and adding to users list
 
 socket.on('user-joined',uname=>{
-      append(`${uname} joined the chat`,'left')
+      append(`${uname} joined the chat`,'left')               //displays the name of the user when joined  in the left side of other participants
 })
 
 form.addEventListener('submit',(e)=>{
       e.preventDefault();
       const message=messageInput.value;
-      append(`You: ${message}`,'right');
+      append(`You: ${message}`,'right');                       //display the message sent by a user in own chatbox in right side 
       socket.emit('send',message);
-      messageInput.value="";
+      messageInput.value="";                                   //empty the input box after sending the essage
 })
-
-
-
 
 socket.on('receive',data=>{
-      append(`${data.uname}:${data.message}`,'left')
+      append(`${data.uname}:${data.message}`,'left')           //displays the message of others in left side
 })
 socket.on('left',name=>{
-      append(`${uname} left the chat`,'left')
+      append(`${uname} left the chat`,'left')                  //shows the name when someone left the chat
 })
 
 
